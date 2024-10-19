3c76e75b64cb074bc051421165892924cfecf9cd
Elmasry <3laaelmasry2005a@gmail.com>
2024-10-19 21:26:07 +0300

    create note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
+
 class NoteModel {
   final String title;
   final String content;
-  final DateTime date;
+  final String date;
+  final int color;
 
-  NoteModel({
-    required this.title,
-    required this.content,
-    required this.date,
-  });
+  NoteModel(
+      {required this.title,
+      required this.content,
+      required this.date,
+      required this.color});
 }
