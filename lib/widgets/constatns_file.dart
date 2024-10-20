f0b97046233e048a41c8fc09104fffd801a4ff2d
Elmasry <3laaelmasry2005a@gmail.com>
2024-10-20 14:07:27 +0300

    inint hive open notes box

--- lib/widgets/constatns_file.dart
+++ lib/widgets/constatns_file.dart
@@ -4,3 +4,5 @@ const Color kPrimaryColor = Color(0xff62fcd7);
 
 double width(context) => MediaQuery.of(context).size.width;
 double height(context) => MediaQuery.of(context).size.height;
+
+const String kNotesBox = "notes_box";
